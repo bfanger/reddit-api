deeae471aff91ff65a56f9fc5468b8546621544a
Bob Fanger <bfanger@gmail.com>
2022-06-21 16:20:43 +0200

    doc: API Types

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,11 +4,37 @@
 
 <section>
   <a href="/swagger-ui">API docs</a>
+  <pre>
+export type ApiGetResponse = {"{"}
+  "r/[topic].json": ListingDto;
+{"}"}
+
+type ListingDto = {"{"}
+  next: string;
+  posts: Array&lt;{"{"}
+    id: string;
+    url: string;
+    title: string;
+    description: string;
+    created: number;
+    thumbnail?: string;
+    video?: string;
+    embed?: string;
+  }{"}"}&gt;
+{"}"}
+  </pre>
 </section>
 
 <style>
   section {
     text-align: center;
     padding: 4rem;
+  }
+  pre {
+    background: #4c4a4a;
+    padding: 1em;
+    width: 30em;
+    margin-inline: auto;
+    text-align: left;
   }
 </style>
