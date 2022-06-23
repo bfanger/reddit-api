69988f0a41f58a7b33711c7b934f449219fa59b9
Bob Fanger <b.fanger@wearetriple.com>
2022-06-23 10:17:33 +0200

    chore: Use node adapter (for heroku)

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,12 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-vercel";
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   preprocess: preprocess({ sourceMap: true }),
   kit: {
     prerender: { default: true },
-    adapter: adapter({
-      edge: false,
-      split: false,
-    }),
+    adapter: adapter(),
     vite: { css: { devSourcemap: true } },
   },
 };
