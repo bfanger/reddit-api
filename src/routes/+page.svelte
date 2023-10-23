850ed1f886f95bb62b88c55a4795c42ebf3adc1f
Bob Fanger <b.fanger@wearetriple.com>
2023-10-23 20:46:28 +0200

    feat: Vercel Demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,10 @@
 <svelte:head>
-  <title>API demo</title>
+  <title>Vercel demo</title>
 </svelte:head>
 
 <section>
-  <a href="/swagger-ui">API docs</a>
   <pre>
-export type ApiGetResponse = {"{"}
-  "r/[topic].json": ListingDto;
-{"}"}
-
-type ListingDto = {"{"}
-  next: string;
-  after: string;
-  posts: Array&lt;{"{"}
-    id: string;
-    url: string;
-    title: string;
-    description: string;
-    created: number;
-    thumbnail?: string;
-    video?: string;
-    embed?: string;
-  {"}"}&gt;
-{"}"}
+    Presentatie demo
   </pre>
 </section>
 
